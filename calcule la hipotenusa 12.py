72d9c6c58e59a4483532dd876ce4d086d70db7dd
josephao64 <103302569+josephao64@users.noreply.github.com>
2022-08-13 10:05:53 -0600

    Add files via upload

---
+++ calcule la hipotenusa 12.py
@@ -0,0 +1,8 @@
+
+
+cateto_a = float (input ('Ingresa el valor de cateto a: '))
+cateto_b = float (input ('Ingresa el valor de cateto b: '))
+hipotenusa=math.sqrt(cateto_a*cateto_a+cateto_b*cateto_b)
+print ('Valor de hipotenusa: ' + repr (hipotenusa))
+print ()
+os.system ('pause')
